1d643855c45399702985bbf7212f712066f45de0
Yevhenii Danoi <geneforapp@gmail.com>
2023-05-10 18:50:52 +0300

    cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 connectDB();
 
@@ -13,9 +15,26 @@ const recordRotues = require("./routes/recordRoutes");
 
 const app = express();
 
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URI,
+  })
+);
+
 app.use(bodyParser.json());
 
 app.use("/api/records", recordRotues);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
